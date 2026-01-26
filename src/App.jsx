0682b6c1c58948aff36ab10458dc74671421b0fa
leeknivoc <leeknivoc@gmail.com>
2026-01-26 14:24:37 +0000

    Context로 모든 컴포넌트에 값 전달을 위한 기본 세팅 및 mockData 삽입

--- src/App.jsx
+++ src/App.jsx
@@ -4,51 +4,41 @@ import Home from "./pages/Home";
 import Diary from "./pages/Diary";
 import New from "./pages/New";
 import Notfound from "./pages/Notfound";
-import { getEmotionImage } from "./util/get-emotion-image";
-import Button from "./components/Button";
-import Header from "./components/Header";
+import Edit from "./pages/Edit";
 
-//1."/" 모든 일기를 조회하는 HOME페이지
-//2. "/new" 새로운 일기를 작성하는 NEW페이지
-//3. "diary" 일기를 상세히 조회하는 Diary 페이지
+import { useReducer } from "react";
 
+const mockData = [
+  //임시데이타
+  {
+    id: 1,
+    createdDate: new Date().getTime(),
+    emotionId: 1,
+    content: "1번 일기 내용",
+  },
+  {
+    id: 2,
+    createdDate: new Date().getTime(),
+    emotionId: 2,
+    content: "2번 일기 내용",
+  },
+];
+
+function reducer(state, action) {
+  return state;
+}
 function App() {
-  const nav = useNavigate();
-  const onClickButton = () => {
-    nav("/new");
-  };
+  const [data, dispatch] = useReducer(reducer, mockData);
+  //새로운 일기 추가
+  //기존 일기 수정
+  //기존 일기 삭제
   return (
     <>
-      <Header
-        title={"Header"}
-        leftChild={<Button text={"Left"} />}
-        rightChild={<Button text={"Right"} />}
-      />
-      <Button
-        text={123}
-        onClick={() => {
-          console.log("123번 버튼 클릭");
-        }}
-        type={"DEFAULT"}
-      />
-      <Button
-        text={123}
-        onClick={() => {
-          console.log("123번 버튼 클릭");
-        }}
-        type={"POSITIVE"}
-      />
-      <Button
-        text={123}
-        onClick={() => {
-          console.log("123번 버튼 클릭");
-        }}
-        type={"NEGATIVE"}
-      />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/new" element={<New />} />
         <Route path="/diary/:id" element={<Diary />} />
+        <Route path="/edit/:id" element={<Edit />} />
         <Route path="*" element={<Notfound />} />
         {/* *은 와일드카드다. 라우트스 태그 안에는 라우트만 들어갈 수 있다. 디브같은거 불가함*/}
       </Routes>
