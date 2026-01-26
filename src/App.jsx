ce45b2fb7fc9a2fe0b6a31cac240e5d84a1ce29b
leeknivoc <leeknivoc@gmail.com>
2026-01-26 10:52:11 +0000

    기본설정 및 페이지별  라우팅

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+import "./App.css";
+import { Route, Routes, Link, useNavigate } from "react-router-dom";
+import Home from "./pages/Home";
+import Diary from "./pages/Diary";
+import New from "./pages/New";
+import Notfound from "./pages/Notfound";
+//1."/" 모든 일기를 조회하는 HOME페이지
+//2. "/new" 새로운 일기를 작성하는 NEW페이지
+//3. "diary" 일기를 상세히 조회하는 Diary 페이지
+
+function App() {
+  const nav = useNavigate();
+  const onClickButton = () => {
+    nav("/new");
+  };
+  return (
+    <>
+      <div>
+        <Link to={"/"}>Home</Link>
+        <Link to={"/new"}>New</Link>
+        <Link to={"/diary"}>Diary</Link>
+      </div>
+      <button onClick={onClickButton}>New 페이지로 이동</button>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/new" element={<New />} />
+        <Route path="/diary" element={<Diary />} />
+        <Route path="*" element={<Notfound />} />
+        {/* *은 와일드카드다. 라우트스 태그 안에는 라우트만 들어갈 수 있다. 디브같은거 불가함*/}
+      </Routes>
+    </>
+  );
+}
+
+export default App;
