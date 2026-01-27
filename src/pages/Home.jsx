1b2975d4402ae03252d37bdeb081b8250e482c2c
leeknivoc <leeknivoc@gmail.com>
2026-01-27 14:50:56 +0000

    Home page 날짜 연동 및 context 적용 및  정렬기능 구현

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,9 +2,37 @@ import Header from "../components/Header";
 import Button from "../components/Button";
 import DiaryList from "../components/DiaryList";
 import { useState } from "react";
+import { useContext } from "react";
+import { DiaryStateContext } from "../App";
+
+const getMonthlyData = (pivotDate, data) => {
+  const beginTime = new Date( //이번달의 첫번째 시작하는 날
+    pivotDate.getFullYear(),
+    pivotDate.getMonth(),
+    1,
+    0,
+    0,
+    0
+  ).getTime();
+
+  const endTime = new Date( //이번달의 마지막 날
+    pivotDate.getFullYear(),
+    pivotDate.getMonth() + 1,
+    0,
+    23,
+    59,
+    59
+  ).getTime();
+
+  return data.filter(
+    (item) => beginTime <= item.createdDate && item.createdDate <= endTime
+  );
+};
 
 const Home = () => {
+  const data = useContext(DiaryStateContext);
   const [pivotDate, setPivotDate] = useState(new Date());
+  const monthlytData = getMonthlyData(pivotDate, data);
 
   const onIncreaseMonth = () => {
     setPivotDate(new Date(pivotDate.getFullYear(), pivotDate.getMonth() + 1));
@@ -20,7 +48,7 @@ const Home = () => {
         leftChild={<Button onClick={onDecreaseMonth} text={"<"} />}
         rightChild={<Button onClick={onIncreaseMonth} text={">"} />}
       />
-      <DiaryList />
+      <DiaryList data={monthlytData} />
     </div>
   );
 };
