80a0be6f719e2562324bcf2fbcecc4e1b428da0f
leeknivoc <leeknivoc@gmail.com>
2026-01-27 13:53:49 +0000

    Home page UI 구현

--- src/App.jsx
+++ src/App.jsx
@@ -81,29 +81,6 @@ function App() {
 
   return (
     <>
-      <button
-        onClick={() => {
-          onCreate(new Date().getTime(), 1, "Hello");
-        }}
-      >
-        일기추가테스트
-      </button>
-
-      <button
-        onClick={() => {
-          onUpdate(1, new Date().getTime(), 3, "수정된 일기입니다.");
-        }}
-      >
-        일기업데이트테스트
-      </button>
-
-      <button
-        onClick={() => {
-          onDelete(1);
-        }}
-      >
-        일기삭제테스트
-      </button>
       <DiaryStateContext.Provider value={data}>
         <DiaryDispatchContext.Provider value={{ onCreate, onUpdate, onDelete }}>
           <Routes>
