e63c6ce06fbd31186d0e2d2903d315aa49d1696f
leeknivoc <leeknivoc@gmail.com>
2026-01-29 14:37:14 +0000

    Diary page 구현 및 삭제하기 기능 오류 개선 및 컴포넌트 분할

--- src/pages/Diary.jsx
+++ src/pages/Diary.jsx
@@ -1,9 +1,37 @@
-import { useParams } from "react-router-dom"; //url의 파라미터 값을 가져오는 훅
+import { useNavigate, useParams } from "react-router-dom"; //url의 파라미터 값을 가져오는 훅
+import Header from "../components/Header";
+import Button from "../components/Button";
+import Viewer from "../components/Viewer";
+import { useContext } from "react";
+import useDiary from "../hooks/useDiary";
+
+import { getStringedDate } from "../util/get-stringed-date";
 
 const Diary = () => {
   const params = useParams();
+  const nav = useNavigate();
+  const curDiaryItem = useDiary(params.id);
+
+  if (!curDiaryItem) {
+    return <div>데이터 로딩중</div>;
+  }
+  console.log(curDiaryItem);
+
+  const { createdDate, emotionId, content } = curDiaryItem;
+  const title = getStringedDate(new Date(createdDate));
   console.log(params);
-  return <div>{params.id}번 일기입니다</div>;
+  return (
+    <div>
+      <Header
+        title={`${title} 기록`}
+        leftChild={<Button onClick={() => nav(-1)} text={"뒤로가기"} />}
+        rightChild={
+          <Button onClick={() => nav(`/edit/${params.id}`)} text={"수정하기"} />
+        }
+      />
+      <Viewer emotionId={emotionId} content={content} />
+    </div>
+  );
 };
 
 export default Diary;
